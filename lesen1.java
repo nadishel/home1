9aba7065670131819e88751d1da2139ee0be3da6
nadi <131201980+nadishel@users.noreply.github.com>
2023-05-02 19:45:26 +0200

    Add files via upload

---
+++ lesen1.java
@@ -0,0 +1,22 @@
+package home1;
+
+public class lesen1 {
+    public static void main(String[]args){
+        char a ='G';
+        int b =89;
+        byte c =4;
+        short d =56;
+        float e =4.7333436F;
+        double f =4.355453532;
+        long g =12121;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+    }
+}
---
+++ lessen2.java
@@ -0,0 +1,13 @@
+package home1;
+
+public class lessen2 {
+    public static void main(String[]args){
+        short n=365;
+        int b=(n%10);
+        int c=(n/10%10);
+        int d=(n/100);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
